e7096344ad4520be63a2bfd8705a7e4226b54088
loyayz <loyayz@foxmail.com>
2022-11-08 00:28:40 +0800

    fix(postgres15): syntax error

--- postgres15/init.sh
+++ postgres15/init.sh
@@ -26,21 +26,21 @@ EOSQL
 done
 
 # Create work user and database
-if [ -n $PG_WORK_USER ]; then
-  echo "Create work user: $PG_WORK_USER"
-  if [ -z $PG_WORK_PASSWORD ]; then
+if [ -n "$PG_WORK_USER" ]; then
+  if [ -z "$PG_WORK_PASSWORD" ]; then
   		cat >&2 <<-'EOE'
-  			Error: Worker password is not specified.
-  			       You must specify PG_WORK_PASSWORD to a non-empty value for the worker.
-  			       For example, "-e PG_WORK_PASSWORD=password" on "docker run".
-  		EOE
-  		exit 1
-  	fi
+Error: Worker password is not specified.
+       You must specify PG_WORK_PASSWORD to a non-empty value for the worker.
+       For example, "-e PG_WORK_PASSWORD=password" on "docker run".
+EOE
+		exit 1
+  fi
 
+  echo "Create work user: $PG_WORK_USER"
   psql -U $POSTGRES_USER -tc "SELECT 1 FROM pg_user WHERE usename = '$PG_WORK_USER'" \
       | grep -q 1 || psql -U $POSTGRES_USER -c "CREATE USER $PG_WORK_USER WITH ENCRYPTED PASSWORD '$PG_WORK_PASSWORD'"
 
-  if [ -n $PG_WORK_DB ]; then
+  if [ -n "$PG_WORK_DB" ]; then
     echo "Create work database: $PG_WORK_DB"
     psql -U $POSTGRES_USER -tc "SELECT 1 FROM pg_database WHERE datname = '$PG_WORK_DB'" \
         | grep -q 1 || psql -U $POSTGRES_USER -c "CREATE DATABASE $PG_WORK_DB WITH TEMPLATE $TEMPLATE_DB OWNER $PG_WORK_USER"
